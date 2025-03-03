8b590995b337490e242e95539dfe4e96d42b0c47
horsedark374 <horsedark374@gmail.com>
2025-03-03 07:43:48 -0600

    Dan

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -2,7 +2,7 @@ const site_url =
   process.env.NEXT_PUBLIC_APP_URL || "https://the-portfolio-lac.vercel.app";
 
 export const siteConfig = {
-  name: "Vova | Senior Full-stack Developer",
+  name: "Danyil | Senior Full-stack Developer",
   description:
     "Personal portfolio website showcasing my projects and skills as a full stack developer",
   url: site_url,
